9c8bf679259829ae146c65e5ccc2c605e3b64125
wiryax <wiryamn08@gmail.com>
2023-08-30 03:55:18 +0700

    Melengkapi language dan memperbaiki tampilan responsive

---
+++ app/Language/en/Pasien/pasien.php
@@ -0,0 +1,43 @@
+<?php
+return [
+    "Welcome"               => "Welcome",
+    "List-Patient"          => "List Patient",
+    "Patient-Code"          => "Patient Code",
+    "Payment-Verif"         => "Payment Verification",
+    "Report"                => "Print Report",
+    "Feedback"              => "Send Feedback",
+    "Logout"                => "Logout",
+    "Examination-Number"    => "Examination Number",
+    "Name"                  => "Patient Name",
+    "Date"                  => "Examination Date",
+    "Examination"           => "Examination",
+    "Diagnosys"             => "Diagnosys",
+    "Recipe-Number"         => "Recepi Code",
+    "Recipe-Detil"          => "Recipe Detil",
+    "Phone"                 => "Phone Number",
+    "Addres"                => "Patient Addres",
+    "Select-Language"       => "Select Language",
+    "List-Patient"          => "List Patient",
+    "Payment-List"          => "Payment-List",
+    "Payment-Date"          => "Payment Date",
+    "Payment-Status"        => "Payment Status",
+    "Report"                => "Report",
+    "Patien-Report"         => "Patient Data Report",
+    "Print"                 => "Print",
+    "Transaction-Number"    => "Transaction Number",
+    "Cost"                  => "Cost",
+    "Proof-Transaction"     => "Proof Of Transaction",
+
+    "Booking"               => "Create New Booking Number",
+    "Set-Date"              => "Set Examination Date",
+    "Examination-List"      => "Your Examination List",
+
+    // sidebar
+    "Booking-Menu"          => "Take Examination Number",
+    "Examination-Result"    => "Examination Result",
+    "Payment"               => "Payment",
+    "Feedback"              => "Send Feedback",
+    "Log-Out"               => "Log Out",
+
+
+];
